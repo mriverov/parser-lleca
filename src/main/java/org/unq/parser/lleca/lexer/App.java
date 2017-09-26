4b1d9a34b686aee1bea3323c0da49575438deb31
Marcia Tejeda <tejedamarcia@gmail.com>
2017-09-26 18:59:45 -0300

    branch development

---
+++ src/main/java/org/unq/parser/lleca/lexer/App.java
@@ -0,0 +1,14 @@
+package org.unq.parser.lleca.lexer;
+
+/**
+ * Created by mtejeda on 26/09/17.
+ */
+public class App {
+
+    /*Sólo para probar voy a crear un lexer con las palabras reservadas de lleca*/
+    int main(){
+
+        return 0;
+    }
+
+}
