0fa74f50d17ac44ee92d479ff0bcde785d7187df
mriverov <mriverov3@gmail.com>
2017-10-15 23:14:02 -0300

    wip: statring parser

--- src/main/java/org/unq/parser/lleca/grammar/lleca/model/Parser.java
+++ src/main/java/org/unq/parser/lleca/grammar/lleca/model/Parser.java
@@ -1,4 +1,18 @@
 package org.unq.parser.lleca.grammar.lleca.model;
 
+import org.unq.parser.lleca.lexer.tokens.Token;
+
+import java.util.List;
+
 public class Parser {
+
+    private List<Token> token;
+
+    public Parser(List<Token> tokens){
+        this.token = tokens;
+    }
+
+    public Grammar parse(){
+        return null;
+    }
 }
